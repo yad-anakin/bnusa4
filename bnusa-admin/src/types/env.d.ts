4e1000820c060a5750950b9b808a5de2123c8c2d
yad-anakin <yadvader88@gmail.com>
2025-07-20 18:40:24 +0300

    Initial push of bnusa folder only

--- bnusa-admin/src/types/env.d.ts
+++
@@ -1,7 +0,0 @@
-declare namespace NodeJS {
-  interface ProcessEnv {
-    readonly NODE_ENV: 'development' | 'production' | 'test';
-    readonly NEXT_PUBLIC_API_URL: string;
-    readonly NEXT_PUBLIC_SITE_URL: string;
-  }
-} 
--- bnusa/src/app/privacy-policy/page.tsx
+++
@@ -1,5 +0,0 @@
-import { redirect } from 'next/navigation';
-
-export default function PrivacyPolicy() {
-  redirect('/terms-of-use');
-} 
